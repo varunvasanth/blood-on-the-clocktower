7427aa72a5bb26f2cd07b76ae70796f8acc201b0
Varun Vasanth <varunvasanth28@gmail.com>
2026-09-13 15:27:30 -0400

    Client message 2

--- everything.js
+++ everything.js
@@ -1,6 +1,6 @@
 async function submitPrompt() {
     let name = document.getElementById("name-input").value;
-    const theURL = `https://miniature-computing-machine-xgwqxqgrgw73pjrp-3874.app.github.dev/potatoes?name={name}`;
+    const theURL = `https://miniature-computing-machine-xgwqxqgrgw73pjrp-3874.app.github.dev/potatoes?name=${name}`;
     const response = await fetch(theURL);
     console.log(response);
     const data = await response.json();
