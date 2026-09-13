b52023805abac2c3c9505a8b2eb501a5a12d20f5
Varun Vasanth <varunvasanth28@gmail.com>
2026-09-13 15:21:50 -0400

    First client message

--- everything.js
+++ everything.js
@@ -1,9 +1,10 @@
 async function submitPrompt() {
-    const theURL = 'https://miniature-computing-machine-xgwqxqgrgw73pjrp-3874.app.github.dev/potatoes';
+    let name = document.getElementById("name-input").value;
+    const theURL = `https://miniature-computing-machine-xgwqxqgrgw73pjrp-3874.app.github.dev/potatoes?name={name}`;
     const response = await fetch(theURL);
     console.log(response);
     const data = await response.json();
     console.log(data);
-    document.getElementById("server-response").innerText = "Their name is " + data.name + ", they are " + data.age + "lightyears old.";
+    document.getElementById("server-response").innerText = "Their name is " + data.name + ", they are " + data.age + " lightyears old.";
 
 }
--- server/backend.js
+++ server/backend.js
@@ -12,19 +12,35 @@ const PORT = 3874;
 // Enable CORS for your specific frontend domain
 app.use(cors({ origin: ['https://varunvasanth.github.io', 'http://127.0.0.1:3000'] }));
 
+// Define the data list
+let dataList = [{
+    name: 'Arthur',
+    age: 42
+},{
+    name: 'Ford',
+    age: 391234
+},{
+    name: 'Earth',
+    age: -1
+}]
+
 // Define a basic route for the root URL ("/")
 function respondToACall(req, res) {
-    let numberThing = req.query.a;
-    res.json({
-        name: 'Arthur',
-        age: 42
-    });
+    let name = req.query.name;
+    let clientResponse = { name:"Sorry, that name isn't defined.", age: 'NAN'};
+    for(let i = 0;i<dataList.length;i++){
+        if(dataList[i].name === name){
+            clientResponse = dataList[i]
+        }
+    }
+    
+    res.json(clientResponse);
 }
 
 app.get('/potatoes', respondToACall);
 
 // Start the server and listen on the specified port
 function serverStartCallback() {
-    console.log(`Server is successfully running at https://localhost:${PORT}`);
+    console.log(`Server is successfully running at https://localhost:${PORT}, so thanks`);
 }
 app.listen(PORT, serverStartCallback);
