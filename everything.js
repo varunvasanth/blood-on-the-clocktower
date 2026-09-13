af56e154dad21c83b5655f3c1c8dd64356565027
Varun Vasanth <varunvasanth28@gmail.com>
2026-09-12 21:49:44 -0400

    Client-server 3 (FINAL)

--- everything.js
+++ everything.js
@@ -1,5 +1,5 @@
 async function submitPrompt() {
-    const theURL = 'https://zany-parakeet-xgwqxqgrxj7fgjg-3874.app.github.dev/potatoes';
+    const theURL = 'https://miniature-computing-machine-xgwqxqgrgw73pjrp-3874.app.github.dev/potatoes';
     const response = await fetch(theURL);
     console.log(response);
 }
