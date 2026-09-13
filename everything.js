c1941a5856e12fc0ad1a00b59152e45efd618bae
Varun Vasanth <varunvasanth28@gmail.com>
2026-09-12 22:01:33 -0400

    Revert #4

--- everything.js
+++ everything.js
@@ -1,7 +1,5 @@
 async function submitPrompt() {
     const theURL = 'https://miniature-computing-machine-xgwqxqgrgw73pjrp-3874.app.github.dev/potatoes';
     const response = await fetch(theURL);
-    const data = await response.json();
-    console.log(data);
-    alert(data);
+    console.log(response);
 }
